75e9190de273f4be2bde58ef47adafd243d44fa7
balbuquerqxe <brunadaher@icloud.com>
2025-07-12 10:44:02 -0300

    Adaptação do App.jsx para incluir a página de Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, Link } from 'react-router-dom';
 import ListaLivros from './pages/ListaLivros';
 import CadastroLivro from './pages/CadastroLivro';
 import CadastroUsuario from './pages/CadastroUsuario';
+import LoginUsuario from './pages/LoginUsuario';
+
 
 export default function App() {
   return (
@@ -10,12 +12,14 @@ export default function App() {
         <Link to="/" className="text-blue-600 font-semibold">Livros</Link>
         <Link to="/cadastro" className="text-blue-600 font-semibold">Cadastrar Livro</Link>
         <Link to="/cadastro-usuario" className="text-blue-600 font-semibold">Cadastrar Usuário</Link>
+        <Link to="/login" className="text-blue-600 font-semibold">Login</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<ListaLivros />} />
         <Route path="/cadastro" element={<CadastroLivro />} />
         <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
+        <Route path="/login" element={<LoginUsuario />} />
       </Routes>
     </div>
   );
