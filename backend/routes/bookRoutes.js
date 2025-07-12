fee8f38638e3799e4fff16d5207543a872abb54a
balbuquerqxe <brunadaher@icloud.com>
2025-07-12 08:26:08 -0300

    criação do arquivo bookRoutes.js que indica quais funções devem ser chamadas quando certos acessos forem feitos

---
+++ backend/routes/bookRoutes.js
@@ -0,0 +1,30 @@
+// Indica quais funções devem ser chamadas quando certos acessos forem feitos
+
+// Importa o express para criar as rotas
+const express = require('express');
+
+// Lida apenas com as rotas relacionadas aos livros
+const router = express.Router();
+
+// Importa as funções do controller de livros
+const {
+    cadastrarLivro,
+    listarLivros,
+    adotarLivro
+} = require('../controllers/bookController');
+
+// OBS: POST é quando queremos enviar dados para criar algo novo
+
+// OBS: GET é quando queremos obter ou ler dados
+
+// Quando for criado um novo livro (POST)
+router.post('/', cadastrarLivro);
+
+// Quando for listar os livros disponíveis (GET)
+router.get('/', listarLivros);     
+
+// Quando for adotar um livro pelo ID (POST)
+router.post('/:id/adotar', adotarLivro);    
+
+// Exporta o router para ser usado em outros arquivos
+module.exports = router;
